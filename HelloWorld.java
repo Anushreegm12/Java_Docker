ba8518d39ec5b0dc3e34b05048675d56d3c42fd1
Anushreegm12 <3693_anushree@katalystindia.org>
2025-04-02 14:19:54 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
             System.out.println("Hello, World from Java in Docker!");
             try {
                 Thread.sleep(5000);
-            } catch(InterruptException e) {
+            } catch(InterruptedException e) {
                 e.printStackTrace();
             }
         }
